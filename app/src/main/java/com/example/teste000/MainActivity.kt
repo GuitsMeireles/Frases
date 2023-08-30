b54c09c45c3107126c363683f524ec1bee85ee29
SeuHokage <batistaguilherme681@gmail.com>
2023-08-30 02:27:21 -0300

    fifith commit!!!!!!!! in 3days

--- app/src/main/java/com/example/teste000/MainActivity.kt
+++ app/src/main/java/com/example/teste000/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import com.example.teste000.databinding.ActivityMainBinding
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         binding.pokeButton.setOnClickListener {
             val i = Intent(this,PokemonActivity::class.java )
