ec9f28fb2098bf2efe36759ddfd0376be500d4bd
SeuHokage <batistaguilherme681@gmail.com>
2023-08-30 01:10:46 -0300

    Terceira commit ¯ \ _ (ツ) _ / ¯

---
+++ app/src/main/java/com/example/teste000/Phrase.kt
@@ -0,0 +1,11 @@
+package com.example.teste000
+
+    class Phrase {
+        val phrase: String
+        val author: String
+
+        constructor(phrase: String, author: String) {
+            this.phrase = phrase
+            this.author = author
+        }
+    }
--- app/src/main/java/com/example/teste000/SegundaTela.kt
+++ app/src/main/java/com/example/teste000/SegundaTela.kt
@@ -8,18 +8,20 @@ import com.example.teste000.databinding.ActivitySegundaTelaBinding
 
 class SegundaTela : AppCompatActivity() {
 
+    private lateinit var authorTextView: TextView
     private lateinit var binding: ActivitySegundaTelaBinding
     private lateinit var textView: TextView
     private lateinit var button: Button
 
     private val phrases = listOf(
-        "Comece onde você está, use o que você tem e faça o que você pode.",
-        "Coragem é saber o que não temer.",
-        "Conhecer a si mesmo é o começo de toda sabedoria.",
-        "Acredite em milagres, mas não dependa deles.",
-        "Cada segundo é tempo para mudar tudo para sempre.",
-        "Aprenda com o ontem. Viva o hoje. tenha esperança para o amanhã."
+        Phrase("Comece onde você está, use o que você tem e faça o que você pode.","Autor 1"),
+        Phrase("Coragem é saber o que não temer.","Autor 2"),
+        Phrase("Conhecer a si mesmo é o começo de toda sabedoria.", "Autor 3"),
+        Phrase("Acredite em milagres, mas não dependa deles.", "Autor 4"),
+        Phrase("Cada segundo é tempo para mudar tudo para sempre.", "Autor 5"),
+        Phrase("Aprenda com o ontem. Viva o hoje. tenha esperança para o amanhã.", "Autor 6")
     )
+
 
     private var currentIndex = 0
 
@@ -34,16 +36,23 @@ class SegundaTela : AppCompatActivity() {
 
         textView = findViewById(R.id.textView)
         button = findViewById(R.id.button)
+        authorTextView = findViewById(R.id.author_TextView)
 
         button.setOnClickListener {
             if (currentIndex < phrases.size) {
-                textView.text = phrases[currentIndex]
+                val currentFrase = phrases[currentIndex]
+                textView.text = currentFrase.phrase
+                authorTextView.text = currentFrase.author
                 currentIndex++
             } else {
                 currentIndex = 0
 
-
-
+                if (currentIndex < phrases.size) {
+                    val currentFrase = phrases[currentIndex]
+                    textView.text = currentFrase.phrase
+                    authorTextView.text = currentFrase.author
+                    currentIndex++
+                }
         }
     }
 }}
